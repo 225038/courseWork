ba9e4eeb190e69be9b916436ae52824253f3bfed
Вадим <legenda2121@mail.ru>
2018-03-11 22:20:33 +0300

    Registration and authentication added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import {RouterModule} from '@angular/router';
+import {RegistrationService} from "./registration-page/registration.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {MatSliderModule} from "@angular/material";
+import { RegistrationAnswerComponent } from './registration-answer/registration-answer.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import {AuthPageService} from "./auth-page/auth-page.service";
 
+const routes = [
+  {path: 'registration', component: RegistrationPageComponent},
+  {path: 'regInfo', component: RegistrationAnswerComponent},
+  {path: 'auth', component: AuthPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent,
+    RegistrationAnswerComponent,
+    AuthPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes), BrowserModule,
+    RouterModule.forRoot(routes), HttpClientModule , FormsModule
   ],
-  providers: [],
+  providers: [RegistrationService, AuthPageService],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
